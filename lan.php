f05c03f06a1bd8fa7bf5a76c0416c231d476ca2a
塵世不再 <asundust@qq.com>
2023-02-21 11:39:04 +0800

    加入局域网IP段的判断，兼容低版本PHP

--- lan.php
+++ lan.php
@@ -1,4 +1,6 @@
 <?php
+
+// CURL请求
 function curl_get($url)
 {
     $headerArray = ["Content-type:application/json;", "Accept:application/json"];
@@ -11,17 +13,43 @@ function curl_get($url)
     return curl_exec($ch);
 }
 
-function startsWith(string $string, string $subString): bool
-{
-    return str_starts_with($string, $subString);
+// 是否局域网IP
+function is_lan($client_ip) {
+    $lanIpSubs = [
+        '10.',
+        '127.',
+        '172.16.',
+        '172.17.',
+        '172.18.',
+        '172.19.',
+        '172.20.',
+        '172.21.',
+        '172.22.',
+        '172.23.',
+        '172.24.',
+        '172.25.',
+        '172.26.',
+        '172.27.',
+        '172.28.',
+        '172.29.',
+        '172.30.',
+        '172.31.',
+        '192.168.',
+    ];
+    foreach ($lanIpSubs as $lanIpSub) {
+        if (strpos($client_ip, $lanIpSub) !== false) {
+            return true;
+        }
+    }
+    return false;
 }
 
+// 内网环境判断
 try {
     $url = 'https://ifconfig.me/ip';
     $server_ip = curl_get($url);
     $client_ip = $_SERVER['REMOTE_ADDR'];
-    //lan:是否是本地
-    $lan = startsWith($client_ip, "192.") || $client_ip == $server_ip;
+    $lan = is_lan($client_ip) || $client_ip == $server_ip;
     $result = ['lan' => $lan, 'ip' => ['client' => $client_ip, 'server' => $server_ip]];
 } catch (Exception $exception) {
     $result = ['lan' => false, 'ip' => ['client' => $client_ip, 'server' => '']];
