d527664382ad10b9d779fcd187c2c3ab0abee00d
asundust <asundust@qq.com>
2023-07-14 17:40:30 +0800

    添加判断是否能访问内网IP逻辑（重新设计）

--- web/can_lan.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$data = [
-    'lan' => true,
-];
-$jsonData = json_encode($data);
-$callback = $_GET['callback'];
-header('Content-Type: application/javascript');
-echo $callback . '(' . $jsonData . ');';
--- web/lan.php
+++ web/lan.php
@@ -45,51 +45,70 @@ function isLanIp($client_ip)
     return false;
 }
 
-// 是否能访问主机IP
-function getLanService()
+// 获取内网地址
+function getLanService($tryLanHttps)
 {
-    return ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://') . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'];
+    // return ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://') . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'];
+    return ($tryLanHttps ? 'https://' : 'http://') . $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'];
 }
 
-// debug
+// debug开关
 $isDebug = true;
 
-$tryLanStatus = true; // 开关（判断是否能访问内网IP，应用场景例如为VPN连入内网）
-// 内网环境判断
-$client_ip = $_SERVER['REMOTE_ADDR'];
-try {
-    // 是否局域网IP
-    $result = isLanIp($client_ip);
-    $result = false;
-    $fun = 'isLanIp';
-    if (!$result) {
-        // 获取主机外网IP判断
-        $url = 'https://ifconfig.me/ip';
-        $server_ip = curl_get($url);
-        $result = $client_ip == $server_ip;
-        $fun = 'clientAndServer';
+// 变量
+$tryLanStatus = true; // 尝试内网访问开关（判断是否能访问内网IP，应用场景例如为VPN连入内网）
+$tryLanHttps = false;  // 尝试内网访问是否HTTPS（暂时无法判断服务段是否HTTPS，采用变量控制，一般内网IP下都是HTTP）
+
+// 函数入口
+$type = isset($_GET['type']) ? $_GET['type'] : 'base';
+if ($type == 'base') {
+    // 客户端IP
+    $client_ip = $_SERVER['REMOTE_ADDR'];
+    try {
+        // 是否局域网IP
+        $result = isLanIp($client_ip);
+        $fun = 'isLanIp';
+        if (!$result) {
+            // 获取主机外网IP判断
+            $url = 'https://ifconfig.me/ip';
+            $server_ip = curl_get($url);
+            $result = $client_ip == $server_ip;
+            $fun = 'clientAndServer';
+        }
+        // 返回消息
+        $result = [
+            'lan' => $result,
+            'ip' => [
+                'client' => $client_ip,
+                'server' => isset($server_ip) ? $server_ip : '',
+            ],
+            'try_can_lan' => [
+                'try_lan_status' => $tryLanStatus,
+                'lan_address' => getLanService($tryLanHttps),
+            ]
+        ];
+        if ($isDebug) {
+            $result['fun'] = $fun;
+        }
+    } catch (Exception $exception) {
+        // 默认消息
+        $result = ['lan' => false];
+        if ($isDebug) {
+            $result['exception'] = $exception->getMessage();
+        }
     }
-    // 返回消息
-    $result = [
-        'lan' => $result,
-        'ip' => [
-            'client' => $client_ip,
-            'server' => isset($server_ip) ? $server_ip : '',
-        ],
-        'try_can_lan' => [
-            'try_lan_status' => $tryLanStatus,
-            'lan_address' => getLanService(),
-        ]
+    die(json_encode($result));
+} else if ($type == 'try_lan') {
+    // 能访问代表为内网服务可用
+    $data = [
+        'lan' => true,
     ];
-    if ($isDebug) {
-        $result['fun'] = $fun;
-        $result['111'] = $_SERVER;
-    }
-} catch (Exception $exception) {
-    // 默认消息
-    $result = ['lan' => false, 'ip' => ['client' => $client_ip, 'server' => '']];
-    if ($isDebug) {
-        $result['exception'] = $exception->getMessage();
-    }
+    $jsonData = json_encode($data);
+    $callback = $_GET['callback'];
+    header('Content-Type: application/javascript');
+    echo $callback . '(' . $jsonData . ');';
+} else {
+    // 其他情况均为错误
+    $result = ['error_message' => 'Type类型错误'];
+    die(json_encode($result));
 }
-die(json_encode($result));
